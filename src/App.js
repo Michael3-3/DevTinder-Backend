af7712d03dcf44c680010c898ff20689952b1790
mikhel <mikhelpottella@gmailcom>
2025-07-04 14:42:53 +0530

    exploring the get,post,use functions for http request and learning how to use postman

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
   
+
+app.get("/user",(req,res)=>{
+    res.send("Hello from DevTinder User");
+});
+
+app.post("/user",(req,res)=>{
+    res.send("User created successfully");
+});
+
+app.use("/user",(req,res)=>{
+    res.send("User route is being used");
+});
 app.use("/home",(req,res)=>{
     res.send("Hello from DevTinder Backend");
 })
-
+   
 app.use("/api",(req,res)=>{
     res.send("Hello from DevTinder API");
 });
