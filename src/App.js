9f2bac79447747e1ae14bf7bea589c751c15c078
mikhel <mikhelpottella@gmailcom>
2025-07-07 14:32:41 +0530

    Fix signup response to include user data and improve error handling in user routes

--- src/App.js
+++ src/App.js
@@ -24,10 +24,33 @@ app.post('/signup',async (req,res,next)=>{
     try {
         await user.save();
         console.log("User created successfully:", user);
-        res.status(201).json({message: "User created successfully"});
+        res.status(201).send({message: "User created successfully", user});
     } catch (error) {   
         console.error("Error creating user:", error);
+        res.status(500).send(json({message: "Internal server error"}));
+    }
+});
+
+app.get('/feed', async (req, res) =>{
+    try{
+        const users = await User.find({});
+        res.send({users});
+    }
+    catch (error) {
+        console.error("Error fetching users:", error);
         res.status(500).json({message: "Internal server error"});
     }
-   
+} );
+
+app.get('/user/:id', async (req, res) => {
+    try {
+        const user = await User.findOne({email : req.params.id});
+        if (!user) {
+            return res.status(404).send("User not found");
+        }
+        res.json(user);
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({message: "Internal server error"});
+    }
 });
